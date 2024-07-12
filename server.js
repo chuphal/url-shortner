1016b0214ead4905249af8fc5385d0c3ea2ab0d1
Chandan <91324501+chuphal@users.noreply.github.com>
2024-07-12 12:50:29 +0530

    added .gitignore file

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ import cors from "cors";
 import xss from "xss-clean";
 import rateLimiter from "express-rate-limit";
 
-// import cookieParser from "cookie-parser";
 import async_error from "express-async-errors";
-// import expressWinston from "express-winston";
 import express from "express";
 import path from "path";
 const __dirname = path.resolve();
@@ -18,8 +16,6 @@ import connectDB from "./db/dbConfig.js";
 
 // import routes.
 import urlRouter from "./routes/url.js";
-// import recipeRouter from "./routes/recipe.js";
-// import { requestLogger, logger } from "./utils/logger.js";
 
 //errors handlers.
 import { notFoundMiddleware } from "./middlewares/not-found.js";
@@ -38,31 +34,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-// app.use(
-//   expressWinston.logger({
-//     winstonInstance: requestLogger,
-//     statusLevels: true,
-//   })
-// );
-// app.use(cookieParser());
 
 app.get("/", async (req, res) => {
   res.send("Hello world");
 });
 
 app.use("/api/v1/url", urlRouter);
-// app.use("/api/v1/recipe", recipeRouter);
-
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
-
-// app.use(
-//   expressWinston.errorLogger({
-//     winstonInstance: logger,
-//   })
-// );
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
